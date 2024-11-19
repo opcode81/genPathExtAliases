b16ce60acd085ad6c396dfaa8fbf196b71c8b916
Dominik Jain <dominik.jain@outlook.com>
2024-11-19 21:40:18 +0100

    Blacklist alias 'eval' and also certain folders, avoiding the generation of aliases for their contents

--- genPathExtAliases.py
+++ genPathExtAliases.py
@@ -3,18 +3,38 @@ import os
 import re
 import sys
 
+BLACKLIST_DIRNAME_REGEX = ["Miniconda.*", ".conda"]
+BLACKLIST_ALIAS = ["eval"]
+
 if __name__ == '__main__':
 	args = sys.argv[1:]
 	if len(args) != 1 or args[0] not in ("msys", "cygwin"):
 		print("usage: genPathextAliases <cygwin|msys>")
 		sys.exit(1)
-	with open("pathextAliases.sh", "wb") as f:
-		for ext in map(str.lower, os.getenv("PATHEXT").split(";")):
-			if ext in (".exe", ".com"): 
+	extensions_to_process = [s.lower() for s in os.getenv("PATHEXT").split(";")]
+	print("Processing extensions %s" % extensions_to_process)
+	with open("pathextAliases.sh", "w") as f:
+		paths = os.getenv("PATH").split(os.path.pathsep)
+		for path in paths:
+			print("Processing path %s" % path)
+			if not os.path.exists(path):
 				continue
-			paths = os.getenv("PATH").split(os.path.pathsep)
-			for path in paths:
-				if not os.path.exists(path):
+
+			# split path into parts and check if any part matches the blacklist
+			blacklisted = False
+			for part in path.split(os.path.sep):
+				for regex in BLACKLIST_DIRNAME_REGEX:
+					if re.match(regex, part):
+						blacklisted = True
+						break
+				if blacklisted:
+					print("Skipping blacklisted path %s" % path)
+					break
+			if blacklisted:
+				continue
+
+			for ext in extensions_to_process:
+				if ext in (".exe", ".com"):
 					continue
 				for file in os.listdir(path):
 					if file[-len(ext):].lower() == ext:
@@ -27,4 +47,9 @@ if __name__ == '__main__':
 							p = re.sub(r"/cygdrive/(\w)", r"\1:", p)
 							alias = file[:-len(ext)]
 							cmd = ('cmd /c "%s"' % p).replace('\\', '/')
-						f.write("alias %s='%s'\n" % (alias, cmd))
+						if alias in BLACKLIST_ALIAS:
+							print("Skipping alias %s" % alias)
+							continue
+						alias_definition = "alias %s='%s'\n" % (alias, cmd)
+						print("Adding %s" % alias_definition.strip())
+						f.write(alias_definition)
